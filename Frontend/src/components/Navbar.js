6f1e1dacf5d98fb87489a2a495f52169acea6b5e
Aarias11 <108088290+Aarias11@users.noreply.github.com>
2023-08-08 19:56:48 -0400

    Add files via upload

---
+++ Frontend/src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Navbar() {
+  return (
+    <nav className='flex justify-between border-b border-cyan-950 p-6 items-center'>
+        <Link to='/'><h2 className='text-3xl font-bold text-orange-600'>techBLOGS</h2></Link>
+        <ul className='flex gap-7'>
+            <Link to='/createblog'><li>Create a Blog</li></Link>
+            <li>Features</li>
+            <li>About Us</li>
+            <li>Contact Us</li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Navbar
